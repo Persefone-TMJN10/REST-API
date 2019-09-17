6fc9a92743eb1f5e2f910adb09507e9b45907126
Dasro <maxx_b@hotmail.com>
2019-09-18 00:06:03 +0200

    ADD: DAL with db.js and connection to MySQL-db

---
+++ app-api/src/dal/db.js
@@ -0,0 +1,15 @@
+/* NPM-PACKAGES */
+import mysql from 'mysql'
+
+const MYSQL_OPTIONS = {
+    host: 'mysql-db',
+    user: 'root',
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    dateStrings: true
+}
+
+var connection = mysql.createConnection(MYSQL_OPTIONS)
+
+/* EXPORT */
+export default connection
